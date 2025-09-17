25d4b776c85c6457cfd3170b984dfa39596382d1
its-steo <steomustadd@gmail.com>
2025-09-17 15:12:04 +0300

    add new chnages

--- frontend/grandview/lib/auth.ts
+++ frontend/grandview/lib/auth.ts
@@ -1,4 +1,5 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api"
+//const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api"
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://grandview-shop.onrennder.com/api"
 
 export interface User {
   id: number
