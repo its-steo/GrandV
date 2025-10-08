7d7ce7c3fd4a0b9cea572017194a6ebf8ddb3b70
its-steo <steomustadd@gmail.com>
2025-10-08 21:16:16 +0300

    add new chnages

--- frontend/grandview/next.config.ts
+++ frontend/grandview/next.config.ts
@@ -19,13 +19,13 @@ const nextConfig: NextConfig = {
       {
         protocol: 'http',
         hostname: 'localhost',
-        port: '3000',
+        port: '8000',
         pathname: '/images/**',
       },
       {
         protocol: 'http',
         hostname: '127.0.0.1',
-        port: '3000',
+        port: '8000',
         pathname: '/images/**',
       },
     ],
